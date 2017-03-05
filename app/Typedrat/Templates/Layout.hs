ca7cccf85cf2d415281a4a9063775eef4d27a6de
Alexis Williams <sasinestro@gmail.com>
2017-03-05 11:21:28 -0800

    Improve design a bit, now I'm gonna focus on actual technical stuff.

--- app/Typedrat/Templates/Layout.hs
+++ app/Typedrat/Templates/Layout.hs
@@ -10,7 +10,7 @@ header username path = header_ . div_ [class_ "header"] $ do
     span_ [class_ "header-at"] "@"
     "typedr.at"
     span_ [class_ "header-pathsep"] " : "
-    "~"
+    toHtml $ path
     span_ [class_ "header-prompt"] " > "
     span_ [class_ "header-command"] "ls"
 
@@ -21,6 +21,8 @@ testLayout :: Html ()
 testLayout = doctypehtml_ $ do
     head_ $ do
         link_ [rel_ "stylesheet", href_ "/static/out/css/all.css"]
+        script_ [src_ "https://use.typekit.net/gex0hqe.js"] ""
+        script_ "try{Typekit.load({ async: true });}catch(e){}"
         title_ "guest@typedr.at : ~ >"
 
     body_ [class_ "dark"] $ do
