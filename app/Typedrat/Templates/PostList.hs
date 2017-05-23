e9284008b3d261922df80bfa8e98f550edbf657d
Alexis Williams <sasinestro@gmail.com>
2017-05-22 17:29:53 -0700

    Add post bodies to landing.

--- app/Typedrat/Templates/PostList.hs
+++ app/Typedrat/Templates/PostList.hs
@@ -5,18 +5,21 @@ import qualified Data.Text as T
 import Data.Time
 import Lucid
 import Typedrat.DB
+import Typedrat.DB.Post
 import Typedrat.Routes
 
 import Typedrat.Templates.Types
 
 postListTemplate :: TVContains a "posts" [(BlogPost Hask, Int64)] xs => RatTemplate xs ()
-postListTemplate = askVar (K :: Key "posts") >>= ul_ [class_ "post-list"] . mapM_ (uncurry listItem)
+postListTemplate = askVar (K :: Key "posts") >>= section_ [class_ "post-list"] . mapM_ (uncurry listItem)
     where
         listItem :: BlogPost Hask -> Int64 -> RatTemplate ys ()
-        listItem post numComments = li_ $ do
+        listItem post numComments = article_ [class_ "post"] $ do
             a_ [href_ (renderPostUrl post)] . h1_ . toHtml . _postTitle $ post
             p_ [class_ "post-dateline"] $ do
                 toHtml . formatTime defaultTimeLocale "%B %e, %Y" . _postTime $ post
                 " – "
                 a_ [href_ $ renderPostUrl post `T.append` "#comments"] . toHtml $
                     (show numComments) ++ " comments"
+            let Right body = renderPostBodyToHtml post
+            section_ body
